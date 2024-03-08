2bbe2e6c302ea57091b6e06a59f9184fc1bb84a1
laeti <laetitia_sfeir@hotmail.com>
2024-03-08 19:22:15 +0100

    n

--- main.py
+++ main.py
@@ -4,20 +4,30 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    portfolio = pd.read_csv(r'input\crisis_portfolio.csv',delimiter=",",index_col="Date",parse_dates=["Date"])
+    # get data -> repository
+    portfolio = pd.read_csv(r'input\crisis_portfolio.csv', delimiter=",", index_col="Date", parse_dates=["Date"])
 
+    # loc -> model
+    # dates -> get_begin_date + get_begin_date --> respository
     # Select portfolio asset prices for the middle of the crisis, 2008-2009
+    # date dans fihcier config
     assetPrices = portfolio.loc['2008-01-01':'2009-12-31']
 
+
     # Plot portfolio's asset prices during this time
+    # plot -> view
     assetPrices.plot().set_ylabel('Closing Prices, USD')
     plt.show()
 
     # Compute the portfolio's daily returns
+    # compute -> view
     assetReturns = assetPrices.pct_change()
+    # get_weights--> repository
+    weights = [0.25, 0.25, 0.25, 0.25]
+    # compute --> model
+    portfolio_returns = assetReturns.dot(weights)
 
-    weights=[0.25,0.25,0.25,0.25]
-    portfolio_returns=assetReturns.dot(weights)
+
 
     # Plot portfolio returns
     portfolio_returns.plot().set_ylabel("Daily Return, %")
@@ -29,7 +39,7 @@ def main():
     # Annualize the covariance using 252 trading days per year
     Covariance = Covariance * 252
 
-    # Display the covariance matrix
+    # Display the covariance matrix --> view
     print(f'Covariance={Covariance}')
 
     portfolio_variance = np.transpose(weights) @ Covariance @ weights
@@ -39,16 +49,14 @@ def main():
     # Calculate the 30-day rolling window of portfolio returns
     returns_windowed = portfolio_returns.rolling(30)
 
+# model
     # Compute the annualized volatility series
     volatility_series = returns_windowed.std() * np.sqrt(252)
-
+# view
     # Plot the portfolio volatility
     volatility_series.plot().set_ylabel("Annualized Volatility, 30-day Window")
+    # view
     plt.show()
-
-
-
-
 
 
 if __name__ == "__main__":
